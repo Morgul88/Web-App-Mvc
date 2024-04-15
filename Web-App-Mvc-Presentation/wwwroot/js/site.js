94ab84c7001ce46c3f58234bd961c3b5462e8bd6
Henrik <staranderh@gmail.com>
2024-04-15 20:17:58 +0200

    håller på med responsivtet

--- Web-App-Mvc-Presentation/wwwroot/js/site.js
+++ Web-App-Mvc-Presentation/wwwroot/js/site.js
@@ -87,6 +87,18 @@ function updateCourseByFilter() {
     
 }
 
+//togglar mellan menuboxen
+document.addEventListener('DOMContentLoaded', function () {
+    var littleBox = document.getElementById('little-box');
+    var menuBar = document.getElementById('menu-bar');
+
+    littleBox.addEventListener('click', function () {
+        menuBar.classList.toggle('active');
+        console.log('Menu bar is active:', menuBar.classList.contains('active'));
+    });
+    
+});
+
 
 //här är contact javascript
 document.addEventListener('DOMContentLoaded', function () {
